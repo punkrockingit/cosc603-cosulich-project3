4092e5e65756123ac34065ccf98de4ca58f0d0c5
punkrockingit <william.cosulich@gmail.com>
2016-04-03 15:57:44 -0400

    Task 3
    I fixed the issue with constructor in point.java that had this.x = y. I
    also changed code in rectangle.java to improve getDiagonal() and
    getArea() methods.

--- Rectangle/src/edu/towson/cis/cosc603/project4/rectangle/Rectangle.java
+++ Rectangle/src/edu/towson/cis/cosc603/project4/rectangle/Rectangle.java
@@ -21,20 +21,41 @@ public class Rectangle {
 	}
 	
 	/**
-	 * Gets the area.
-	 *
-	 * @return the area
-	 */
-	public Double getArea() {
-		return Math.abs((p2.x - p1.x) * (p2.y - p1.y));
-	}
-	
-	/**
-	 * Gets the diagonal.
-	 *
-	 * @return the diagonal
-	 */
-	public Double getDiagonal() {
-		return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
-	}
+	* This calculates the area of a rectangle and returns it as a double.
+	   * @return area of rectangle
+	   */
+	   public Double getArea() {
+		    // To hold the absolute width value.
+			double width;
+			// To hold the absolute height value. 
+			double height;
+			// To hold the area value.
+			double area;
+			
+			width = Math.abs(p2.x - p1.x);
+			height = Math.abs(p2.y - p1.y);
+			area = width * height;
+			return area;
+	   }
+	   
+	   /**
+	   * This calculates the diagonal of a rectangle and returns it as a double.
+	   * @return diagonal of rectangle
+	   */
+	   public Double getDiagonal() {
+			// To hold the squared width value of the rectangle.
+			double widthSquared;
+			// To hold the squared height value of the rectangle. 
+			double heightSquared;
+			
+			widthSquared = Math.pow((p2.x - p1.x), 2);
+			heightSquared = Math.pow((p2.y - p1.y), 2);
+			
+			// To hold the diagonal value of the rectangle.
+			double diagonal;
+			diagonal = Math.sqrt(widthSquared + heightSquared);
+			
+			return diagonal;
+	   }
+
 }
